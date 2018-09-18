3dd12620eed21111a137908cf45b22e4f1f05c77
Fede Alessio <alessiofederico@hotmail.com>
2018-09-18 12:09:02 -0300

    Arduino: Servo motor open/close

--- Arduino/safe-door-um/safe-door-um.ino
+++ Arduino/safe-door-um/safe-door-um.ino
@@ -1,21 +1,47 @@
-int led4 = 4;
-int estado = 0;
+#include <Servo.h>
+
+int ledPin = 4;
+int servoPin = 3;
+int servoOpen = 30;
+int servoClose = 120;
+Servo servoInstance;
+int servoPosition;
+int btMessage;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(led4, OUTPUT);
+  Serial.begin(9600);  
+  pinMode(ledPin, OUTPUT);
+  servoInstance.attach(servoPin); // 650, 2550
+  delay(250);
+
+  servoPosition = servoInstance.read();
+  if(servoPosition < servoClose) {
+    servoInstance.write(servoClose);
+  }
+  Serial.println(servoPosition);
 }
 
 void loop() {
-  if ( Serial.available() > 0) {
-    estado = Serial.read();
+  if (Serial.available() > 0) {
+    btMessage = Serial.read();
   }
-  switch (estado) {
+  switch (btMessage) {
     case 'a':
-      digitalWrite(led4, HIGH);
+      if(servoPosition > servoOpen) {
+        servoInstance.write(servoOpen);
+        delay(200);
+        servoPosition = servoInstance.read();
+        digitalWrite(ledPin, HIGH);
+      }
       break;
     case 'b':
-      digitalWrite(led4, LOW);
+      if(servoPosition < servoClose) {
+        servoInstance.write(servoClose);
+        delay(200);
+        servoPosition = servoInstance.read();
+        digitalWrite(ledPin, LOW);
+      }
       break;
   }
+  delay(250);
 }
